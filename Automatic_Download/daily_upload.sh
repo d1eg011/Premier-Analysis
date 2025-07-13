0e28633e7f9032a08a1dd8eedd83a68b795ad295
d1eg011 <dngc4993@gmail.com>
2025-07-12 23:07:13 -0600

    Added automatic upload of daily data to GitHub and removed cron error message txt file

---
+++ Automatic_Download/daily_upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Go to your repo directory
+cd .. || exit 1
+
+# Add all changes
+git add .
+
+# Commit with today's date (only if there are changes)
+git diff --cached --quiet || git commit -m "Auto update on $(date '+%Y-%m-%d')"
+
+# Push to GitHub
+git push origin main
